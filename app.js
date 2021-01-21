903e73a8d4ad2c095ed754fc75437ea4ab20eb27
verstraetetycho <tycho.verstraete@student.ehb.be>
2021-01-21 17:24:00 +0100

    small styling changes + intro page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const URL = process.env.APP_URL
 const expressLayouts = require('express-ejs-layouts')
 const generalRouter = require('./routes/main.router')
 const authRouter = require('./routes/auth.router')
+const apiRouter = require('./routes/api.router')
 
 app.set('view engine', 'ejs')
 app.use(expressLayouts)
@@ -16,9 +17,12 @@ app.use(express.urlencoded({ extended: false }))
 // Static Files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/fonts', express.static(__dirname + 'public/fonts'))
 
 app.use(generalRouter)
 app.use('/auth', authRouter)
+app.use('/api', apiRouter)
 
 app.listen(port, () => {
   console.log(`Listening on ${URL}:${port}`)
--- routes/api.router.js
+++ routes/api.router.js
@@ -1,26 +1,8 @@
 'use strict'
-const User = require('../models/User')
-const UserController = require('../controllers/UserController')
+
 const express = require('express')
 const router = express.Router()
 const cors = require('cors')
-
-const userController = new UserController()
-
-// // Routes
-router.options('*', (req, res) => {
-  if (req.headers.origin == null) {
-    res.status(400).json({ error: 'Origin header missing' })
-  } else {
-    res
-      .header('Access-Control-Allow-Origin', `${req.headers.origin}`)
-      .header('Access-Control-Allow-Headers', '*')
-      .header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-      .header('Vary', 'Origin')
-      .status(200)
-      .json({ success: true, headers: req.headers })
-  }
-})
 
 router.use((req, res, next) => {
   if (
@@ -35,40 +17,18 @@ router.use((req, res, next) => {
   }
 })
 
-router.get('/', (req, res) => {
-  userController.test().then((result) => {
-    res.status(200).json(result)
-  })
-})
-
-router.get('/users', (req, res) => {
-  userController.getUsers().then((users) => {
-    res.status(200).json(users)
-  })
-})
-
-router.post('/users/register', (req, res) => {
-  if (req.body.confPassword == req.body.password) {
-    let user = new User(req.body.name, req.body.email, req.body.password)
-    userController.registerUser(user).then((success) => {
-      if (success) {
-        res.status(201).json({
-          success: true,
-          message: 'User saved successfully',
-          user: user
-        })
-      } else {
-        res.status(400).json({
-          success: false,
-          message: 'Something went wrong. User is not saved.'
-        })
-      }
-    })
+// Routes
+router.options('*', (req, res) => {
+  if (req.headers.origin == null) {
+    res.status(400).json({ error: 'Origin header missing' })
   } else {
-    res.status(400).json({
-      success: false,
-      message: 'Passwords must match. User is not saved.'
-    })
+    res
+      .header('Access-Control-Allow-Origin', `${req.headers.origin}`)
+      .header('Access-Control-Allow-Headers', '*')
+      .header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+      .header('Vary', 'Origin')
+      .status(200)
+      .json({ success: true, headers: req.headers })
   }
 })
 
